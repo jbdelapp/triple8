c4a558dec3e662f1d8c15738d96f488eb728119f
Rachel Fritz <rachelfritz0@gmail.com>
2020-07-09 15:24:51 -0500

    added appends to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var apiKey = "d3247a9e8b9789c09ccfc53d466289d4";
 
-function currentWeather(){
+var currentWeather = function (){
   navigator.geolocation.getCurrentPosition(function (position){
     longitude = position.coords.longitude;
     latitude = position.coords.latitude;
@@ -30,7 +30,7 @@ currentWeather();
 
 // 5 day weather forecast
 
-function fiveDayForecast() {
+var fiveDayForecast = function() {
     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=San+Diego&appid=" + apiKey;
 
     $.ajax({
@@ -188,3 +188,56 @@ var icon2url = "http://openweathermap.org/img/w/" + icon4 + ".png";
 
 var icon5 = responseTwo.list[4].weather[0].icon;
 var icon2url = "http://openweathermap.org/img/w/" + icon5 + ".png";
+
+// convert the temperature with below formula
+var tempOneF = (responseTwo.list[4].main.temp - 273.15) * 1.8 + 32;
+var tempOne = tempOneF.toFixed(1);
+var tempTwoF = (responseTwo.list[12].main.temp - 273.15) * 1.8 + 32;
+var tempTwo = tempTwoF.toFixed(1);
+var tempThreeF = (responseTwo.list[20].main.temp - 273.15) * 1.8 + 32;
+var tempThree = tempThreeF.toFixed(1);
+var tempFourF = (responseTwo.list[28].main.temp - 273.15) * 1.8 + 32;
+var tempFour = tempFourF.toFixed(1);
+var tempFiveF = (responseTwo.list[36].main.temp - 273.15) * 1.8 + 32;
+var tempFive = tempFiveF.toFixed(1);
+
+var day1 = responseTwo.list[4].dt_txt;
+var day2 = responseTwo.list[12].dt_txt;
+var day3 = responseTwo.list[20].dt_txt;
+var day4 = responseTwo.list[28].dt_txt;
+var day5 = responseTwo.list[36].dt_txt;
+
+$("#day-1").html("<h5>" + day1.substr(0, 10) + "</h5>");
+    $("#day-1").append("<img src=" + icon1url + ">");
+    $("#day-1").append("<p>" + "Temp: " + tempOne + " °F </p>");
+    $("#day-1").append("<p>" + "Humidity: " + responseTwo.list[4].main.humidity + " % </p>");
+  
+    $("#day-2").html("<h5>" + day2.substr(0, 10) + "</h5>");
+    $("#day-2").append("<img src=" + icon2url + ">");
+    $("#day-2").append("<p>" + "Temp: " + tempTwo + " °F </p>");
+    $("#day-2").append("<p>" + "Humidity: " + responseTwo.list[12].main.humidity + " % </p>");
+  
+    $("#day-3").html("<h5>" + day3.substr(0, 10) + "</h5>");
+    $("#day-3").append("<img src=" + icon3url + ">");
+    $("#day-3").append("<p>" + "Temp: " + tempThree + " °F </p>");
+    $("#day-3").append("<p>" + "Humidity: " + responseTwo.list[20].main.humidity + " % </p>");
+  
+    $("#day-4").html("<h5>" + day4.substr(0, 10) + "</h5>");
+    $("#day-4").append("<img src=" + icon4url + ">");
+    $("#day-4").append("<p>" + "Temp: " + tempFour + " °F </p>");
+    $("#day-4").append("<p>" + "Humidity: " + responseTwo.list[28].main.humidity + " % </p>");
+  
+    $("#day-5").html("<h5>" + day5.substr(0, 10) + "</h5>");
+    $("#day-5").append("<img src=" + icon5url + ">");
+    $("#day-5").append("<p>" + "Temp: " + tempFive + " °F </p>");
+    $("#day-5").append("<p>" + "Humidity: " + responseTwo.list[36].main.humidity + " % </p>");
+  });
+  
+});
+
+var cities = [];
+
+var getCities = function(){
+    var getCity = localStorage.getItem("cities");
+    console.log(getCity);
+}
